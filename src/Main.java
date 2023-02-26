49b730ae110e61f5b2961012382f733bd9cb80f5
KrisHite <alexgusevmail@gmail.com>
2023-02-26 14:41:47 +0500

    Пример

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+        BufferedWriter out = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\My\\MyFile1.txt"), 1024);
+            out = new BufferedWriter(new FileWriter("C:\\My\\MyFile2.txt"));
+            int lineCount = 0;//Счетчик строк
+            String s;
+// Переписывание информации из одного файла в другой
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+                System.out.println(lineCount + ": " + s);
+                out.write(s);
+                out.newLine();
+            }
+        }catch (IOException e) {
+            System.out.println("Ошибка !!!!!!!");
+        }finally{
+                br.close();
+                out.flush();
+                out.close();
     }
+}
 }
